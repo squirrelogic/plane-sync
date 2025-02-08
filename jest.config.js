f9863e209a79e5bebe1def5892a46cbdf0798f7b
Scott Beardsley <scott@beards.ly>
2025-02-08 14:22:40 -0500

    test: configure Jest and add basic test setup

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/'
+  ]
+};
---
+++ src/__tests__/setup.test.ts
@@ -0,0 +1,5 @@
+describe('Test Setup', () => {
+  it('should pass this basic test', () => {
+    expect(true).toBe(true);
+  });
+});
